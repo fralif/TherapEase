57fdae76a24be2ed22f8a14eee4d9df61871ef65
Alif Fitrianto Ramadhan <125931880+fralif@users.noreply.github.com>
2023-06-16 20:55:06 +0700

    Delete app directory

--- app/src/main/java/io/edgeperformance/edge/interfaces/CallbacksT.java
+++
@@ -1,9 +0,0 @@
-package io.edgeperformance.edge.interfaces;
-
-public interface CallbacksT<T> {
-
-    void onSuccess(T data);
-
-    default void onFailure(Throwable throwable) {
-    }
-}
--- app/src/main/java/io/edgeperformance/edge/interfaces/onAlertListener.java
+++
@@ -1,7 +0,0 @@
-package io.edgeperformance.edge.interfaces;
-
-public interface onAlertListener {
-    void onClickYesButton();
-
-    void onClickNoButton();
-}
